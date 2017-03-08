4f9b27fa92c9ac111ca810aeb7debd01e5f1c7c8
spartakusQ <spartakus1688@gmail.com>
2017-03-08 18:13:48 +0300

    Num gueses done

--- get_number.rb
+++ get_number.rb
@@ -1,8 +1,12 @@
 puts "Welcome 'Get my number' "
-print "What your name"
+print "What your name: "
 input = gets
 print "Welcome, #{input}"
 
 puts "i've got a random number between 1 and 100 "
 puts "Can you guess it? "
+
 target = rand(100) + 1
+
+num_gueses = 0
+puts  "You've got #{10 - num_gueses} gueses left. "
