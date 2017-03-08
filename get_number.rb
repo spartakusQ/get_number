e6e272ea7b3be99fed8097c04b57ce2d8fba0238
spartakusQ <spartakus1688@gmail.com>
2017-03-08 18:03:30 +0300

    Add to rand

--- get_number.rb
+++ get_number.rb
@@ -2,3 +2,7 @@ puts "Welcome 'Get my number' "
 print "What your name"
 input = gets
 print "Welcome, #{input}"
+
+puts "i've got a random number between 1 and 100 "
+puts "Can you guess it? "
+target = rand(100) + 1
