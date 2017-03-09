7de092388f34f70ebb1649140a207e9918113d5d
spartakusQ <spartakus1688@gmail.com>
2017-03-09 16:15:24 +0300

    Last correct. Game work.

--- get_number.rb
+++ get_number.rb
@@ -1,12 +1,33 @@
-puts "Welcome 'Get my number' "
-print "What your name: "
-input = gets
-print "Welcome, #{input}"
+print "Как вас зовут? "
+  input = gets
+  name = input.chomp
+puts "Здраствуй, #{name}!"
 
-puts "i've got a random number between 1 and 100 "
-puts "Can you guess it? "
+puts "Попробуй угадать число от 1 до 100."
+puts "Сможешь?"
 
-target = rand(100) + 1
+    target = rand(100)
 
-num_gueses = 0
-puts  "You've got #{10 - num_gueses} gueses left. "
+    guesses = 0
+
+    guessed_it = false
+
+until guesses == 10 || guessed_it
+  puts "У тебя осталось #{10 - guesses} попыток"
+  guess = gets.strip.to_i
+  guesses += 1
+
+  if guess < target
+    puts "Ох ваше число МЕНЬШЕ."
+  elsif guess > target
+    puts "Ох ваше число БОЛЬШЕ."
+  else
+    puts "Хорошая работа, #{name}!"
+    puts "Загаданное число было #{guess}"
+    guessed_it = true
+  end
+end
+
+unless guessed_it
+    puts "Извините вы не угадали число. (Это было #{target}.)"
+end
